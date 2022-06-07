b32142a013e88d813547eee4d43c92724c0100ba
dach83 <dach83@mail.ru>
2022-06-07 11:29:16 +0300

    Initial commit

---
+++ app/src/main/java/com/chernorotov/gb06_popular_library_git_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chernorotov.gb06_popular_library_git_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
