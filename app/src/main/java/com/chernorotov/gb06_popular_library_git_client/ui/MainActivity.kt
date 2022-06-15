b33e6ea080e9214a7689566e2b3bf81aad064368
dach83 <dach83@mail.ru>
2022-06-15 11:19:17 +0300

    Add users layout

--- app/src/main/java/com/chernorotov/gb06_popular_library_git_client/ui/MainActivity.kt
+++ app/src/main/java/com/chernorotov/gb06_popular_library_git_client/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.chernorotov.gb06_popular_library_git_client
+package com.chernorotov.gb06_popular_library_git_client.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chernorotov.gb06_popular_library_git_client.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/chernorotov/gb06_popular_library_git_client/ui/users/UsersFragment.kt
@@ -0,0 +1,7 @@
+package com.chernorotov.gb06_popular_library_git_client.ui.users
+
+import androidx.fragment.app.Fragment
+import com.chernorotov.gb06_popular_library_git_client.R
+
+class UsersFragment : Fragment(R.layout.fragment_users) {
+}
