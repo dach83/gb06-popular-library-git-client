93c81efcb70f9c03fd31b8dc75861f9b24bf976a
dach83 <dach83@mail.ru>
2022-07-01 10:03:15 +0300

    fix RxButton setOnClickListener

--- app/src/main/java/com/chernorotov/gb06_popular_library_git_client/ui/view/RxButton.kt
+++ app/src/main/java/com/chernorotov/gb06_popular_library_git_client/ui/view/RxButton.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class RxButton : AppCompatButton {
 
@@ -24,10 +24,17 @@ class RxButton : AppCompatButton {
     ) : super(context, attrs, defStyleAttr)
 
 
-    val clicks: Observable<View> = Observable.create { emitter ->
-        setOnClickListener { view ->
-            emitter.onNext(view)
-        }
+    val clicks = PublishSubject.create<View>().also {
+        setOnClickListener(null)
+    }
+
+    private fun rxClickListenerWrapper(l: OnClickListener?): (View) -> Unit = { view ->
+        clicks.onNext(view)
+        l?.onClick(view)
+    }
+
+    override fun setOnClickListener(l: OnClickListener?) {
+        super.setOnClickListener(rxClickListenerWrapper(l))
     }
 
 }
